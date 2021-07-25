4f74d92d3eb20a033f292506bc5748f05532168d
Rosano Coutinho <pub@xeari.com>
2021-07-25 12:40:43 -0300

    Fix typos

--- controller.js
+++ controller.js
@@ -6,9 +6,7 @@ const mod = {
 			OLSKRouteMethod: 'get',
 			OLSKRouteSignature: 'OLSKJarStubRoute',
 			OLSKRouteFunction (req, res, next) {
-				return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'main'), Object.fromEntries(Array.from((new URLSearchParams(req.query)).entries()).filter(function ([key, value]) {
-					return value !== 'undefined';
-				})));
+				return res.OLSKExpressLayoutRender(require('path').join(__dirname, 'main'));
 			},
 			OLSKRouteLanguageCodes: ['en', 'fr', 'es', 'pt'],
 		}];
